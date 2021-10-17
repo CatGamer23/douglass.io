e63cf5420022048759610ce7912218911603dfaf
CatGamer23 <lfiller23@gmail.com>
2021-10-16 21:29:53 -0400

    emp[ty

--- server/index.js
+++ server/index.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const process = require("process");
 const fs = require("fs");
+var config = require("../config.json");
 
 // Variables
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || config.port;
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
@@ -13,4 +14,9 @@ app.get("/", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`Douglass.io listening at http://127.0.0.1:${port}`);
+});
+
+io.on("connection", function (socket) {
+  console.log("Somebody connected!");
+  // Write your code here
 });
