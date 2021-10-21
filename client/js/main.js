e292551e31c694a74e5b040436b82ad582d07262
CatGamer23 <lfiller23@gmail.com>
2021-10-21 06:58:33 -0400

    updated css and added audio & images

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,64 @@
 // Imports
+const io = require("socket.io-client");
+const config = require("./config");
 
 // Variables
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-const centerY = canvas.height / 2;
-const centerX = canvas.width / 2;
 
 // Canvas Size
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
+
+// Checks if the nick chosen contains valid alphanumeric characters (and underscores).
+function validNick() {
+  var regex = /^\w*$/;
+  console.log("Regex Test", regex.exec(playerNameInput.value));
+  return regex.exec(playerNameInput.value) !== null;
+}
+
+window.onload = () => {
+  var startButton = document.getElementById("startButton");
+  var spectateButton = document.getElementById("spectateButton");
+  var nickErrorText = document.querySelector("#startMenu .input-error");
+
+  spectateButton.onclick = () => {
+    startGame("spectate");
+  };
+
+  startButton.onclick = () => {
+    console.log("Clicked: " + document.getElementById("playerName").value);
+    // Checks if the nick is valid.
+    if (validNick()) {
+      nickErrorText.style.opacity = 0;
+      startGame("player");
+    } else {
+      nickErrorText.style.opacity = 1;
+    }
+  };
+
+  var settingsMenuButton = document.getElementById("settingsButton");
+  var settingsMenu = document.getElementById("settings");
+
+  settingsMenuButton.onclick = () => {
+    if (settingsMenu.style.maxHeight == "300px") {
+      settingsMenu.style.maxHeight = "0px";
+    } else {
+      settingsMenu.style.maxHeight = "300px";
+    }
+  };
+
+  playerNameInput.addEventListener("keypress", (e) => {
+    var key = e.which || e.keyCode;
+
+    if (key === global.KEY_ENTER) {
+      // Checks if the nick is valid.
+      if (validNick()) {
+        nickErrorText.style.opacity = 0;
+        startGame("player");
+      } else {
+        nickErrorText.style.opacity = 1;
+      }
+    }
+  });
+};
