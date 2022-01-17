cd17b93518f4832bf0bccf75cb30a459159583eb
CatGamer23 <lfiller23@gmail.com>
2022-01-17 10:42:23 -0500

    updated packages

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ export NODE_OPTIONS=--openssl-legacy-provider
 gulp build
 # clear
 node ./bin/server/server.js
-read -p "Press any key to continue... " -n1 -s
+# read -p "Press any key to continue... " -n1 -s
